4f0a35924d5613c9f9dfc964a0ded81842ba877d
hanmoongoo <hanmoongoo@gmail.com>
2021-03-04 15:42:05 +0900

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Router from 'svelte-spa-router'
+    import Header from '~/components/Header.svelte'
+    import routes from '~/routes'
+</script>
+
+<Header />
+<Router {routes} />
+
+
+<style lang="scss"></style>
